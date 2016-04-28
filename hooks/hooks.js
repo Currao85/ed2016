2698af278abe833c07b1b02579a6f86989ffd064
Valerio Ponte <valerio.ponte@gmail.com>
2016-04-28 18:04:19 +0200

    [Profile CRUD] Improve documentation

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -4,7 +4,11 @@ var _ = require('lodash');
 
 var stash = {};
 
-var login = function (transaction, done) {  
+var authenticateTransaction = function (transaction) {
+  transaction.request['headers']['Cookie'] = stash['token'];
+}
+
+hooks.beforeAll(function(transaction, done) {
   request
   .get('http://192.168.2.225/vts-ed-rest-api/secured/login')
   .auth('admin', 'admin')
@@ -14,11 +18,18 @@ var login = function (transaction, done) {
       return;
     }
     stash['token'] = res.header['set-cookie'][0].split(';')[0];
-    transaction.request['headers']['Cookie'] = stash['token'];
   
     done();  
   })
-}
+});
+
+hooks.before("Profiles > Profiles collections > List Profiles", function(transaction, done) {
+  // we have to remove the 'report' parameter or we'll receive binary data
+  var params = transaction.fullPath.split('&');
+  // the 'report' parameter is currently the last one
+  transaction.fullPath = params.splice(0, params.length - 1).join('&');
+  done();
+});
 
 hooks.before("Profiles > Profiles collections > List Vendor Profiles", function(transaction, done) {
   // we have to remove the 'report' parameter or we'll receive binary data
@@ -28,4 +39,26 @@ hooks.before("Profiles > Profiles collections > List Vendor Profiles", function(
   done();
 });
 
-hooks.before("Profiles > Profile > Update Profile", login);
+// re-enable user profile after 
+hooks.after("Profiles > Profile > Dismiss Profile", function(transaction, done) {
+  request
+  .put('http://192.168.2.225/vts-ed-rest-api/profiles/1/dismiss')
+  .set('Cookie', stash['token'])
+  .send({date: null}).end(function(err, res) {
+    if (err) {
+      transaction.fail = err;
+      hooks.log(err);
+      return;
+    }
+    
+    done();
+  });
+});
+
+// set authentication token (cookie) on actions that require authentication
+[
+  "Profiles > Profile > Update Profile",
+  "Profiles > Profile > Dismiss Profile"
+].map(function(action) {
+  hooks.before(action, authenticateTransaction);
+});
